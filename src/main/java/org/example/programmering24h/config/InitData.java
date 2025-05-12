97fadbeee5a5245a35bcbc16e6fba5c67f7aaefa
amaliekea <amje0001@stud.kea.dk>
2025-05-12 18:39:45 +0200

    check if i can trigger workflow

--- src/main/java/org/example/programmering24h/config/InitData.java
+++ src/main/java/org/example/programmering24h/config/InitData.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class InitData implements CommandLineRunner {
-
+//test
     @Override
     public void run(String... args) throws Exception {
 
