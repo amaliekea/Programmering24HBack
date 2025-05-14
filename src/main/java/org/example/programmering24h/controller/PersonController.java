fc282a79c75554262ff90d6a8349c6685d1766aa
amaliekea <amje0001@stud.kea.dk>
2025-05-14 09:31:23 +0200

    init

---
+++ src/main/java/org/example/programmering24h/controller/PersonController.java
@@ -0,0 +1,24 @@
+package org.example.programmering24h.controller;
+
+
+import org.example.programmering24h.model.Person;
+import org.example.programmering24h.repositories.PersonRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+}
