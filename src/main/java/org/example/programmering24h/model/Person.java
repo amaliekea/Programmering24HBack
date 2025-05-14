b3d2ba8ebef2cc4abfe29ec17987d4eeea45a1e6
amaliekea <amje0001@stud.kea.dk>
2025-05-14 14:25:04 +0200

    init

--- src/main/java/org/example/programmering24h/model/Person.java
+++ src/main/java/org/example/programmering24h/model/Person.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 
 @Entity
 public class Person {
